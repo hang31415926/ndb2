ee91d6ccd693407666aa5dcbb7fdaac961e6ee40
hang31415926 <595434734@qq.com>
2019-12-27 15:21:45 +0800

    修复无法调试 npm 命令的 BUG

--- bin/node-inspect.js
+++ bin/node-inspect.js
@@ -1,21 +1,50 @@
 #!/usr/bin/env node
 
-const program = require('commander')
+// process.env.NODE_DEBUG = 'inspect';
 
-program.version(require('../package.json').version);
+let argv = process.argv.slice(2);
 
-process.env.NODE_DEBUG = 'inspect';
+if (argv.length < 1) {
+  help();
+}
 
-program
-  .arguments('<script> [scriptArgs...]')
-  .option('-h, --host <host>', 'host', '127.0.0.1')
-  .option('-p, --port <port>', 'port', 9229)
-  .action((script, scriptArgs, cmd) => {
-    require('../lib/inspect.js').start(script, scriptArgs, cmd)
-  })
+const options = parseArgv(argv);
 
-program.parse(process.argv)
+require('../lib/inspect.js').start(options);
 
-if (!process.argv.slice(2).length) {
-  program.outputHelp()
+function help(code = 1){
+	console.error(`Usage: ndb script.js`);
+  console.error(`       ndb <host>:<port> script.js`);
+  process.exit(code);
+}
+
+function version(code = 0){
+	console.log(require('../package.json').version);
+  process.exit(code);
+}
+
+function parseArgv([target, ...args]) {
+
+  let host = '127.0.0.1';
+  let port = 9229;
+
+  let script = target;
+  let scriptArgs = args;
+
+  const hostMatch = target.match(/^([^:]+):(\d+)$/);
+
+  if (hostMatch) {
+  	if(args.length < 1){
+  		help();
+  	}
+    host = hostMatch[1];
+    port = parseInt(hostMatch[2], 10);
+    script = args.shift();
+  }else if(target == '-h' || target == '--help'){
+  	help(0);
+  }else if(target == '-v' || target == '--version'){
+  	version();
+  }
+  
+  return { host, port, script, scriptArgs };
 }
