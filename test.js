5815aeb7b699adf283a29984b38e8b520ab66676
hang31415926 <595434734@qq.com>
2019-12-27 15:22:43 +0800

    version 1.0.2

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log(__dirname)
+const spawn = require('cross-spawn');
+const { execSync } = require('child_process');
+// const child = spawn('node', ["C:/Program Files/nodejs/node_modules/npm/bin/npm-cli.js", '-v'], {
+//   env: Object.assign({}, process.env, {
+//     NODE_OPTIONS: `--inspect-brk=127.0.0.1:9229`,
+//   }),
+//   stdio: 'inherit'
+// });
+
+let NPM_CLI_JS = execSync('npm prefix -g');
+console.log(NPM_CLI_JS.toString());
