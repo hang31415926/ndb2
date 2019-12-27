e8941e0f38545a12c41ecde55be60d9ea530beb3
hang31415926 <595434734@qq.com>
2019-12-27 15:42:25 +0800

    修改bin文件

---
+++ bin/ndb2.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+// process.env.NODE_DEBUG = 'inspect';
+
+let argv = process.argv.slice(2);
+
+if (argv.length < 1) {
+  help();
+}
+
+const options = parseArgv(argv);
+
+require('../lib/inspect.js').start(options);
+
+function help(code = 1){
+	console.error(`Usage: ndb script.js`);
+  console.error(`       ndb <host>:<port> script.js`);
+  process.exit(code);
+}
+
+function version(code = 0){
+	console.log(require('../package.json').version);
+  process.exit(code);
+}
+
+function parseArgv([target, ...args]) {
+
+  let host = '127.0.0.1';
+  let port = 9229;
+
+  let script = target;
+  let scriptArgs = args;
+
+  const hostMatch = target.match(/^([^:]+):(\d+)$/);
+
+  if (hostMatch) {
+  	if(args.length < 1){
+  		help();
+  	}
+    host = hostMatch[1];
+    port = parseInt(hostMatch[2], 10);
+    script = args.shift();
+  }else if(target == '-h' || target == '--help'){
+  	help(0);
+  }else if(target == '-v' || target == '--version'){
+  	version();
+  }
+  
+  return { host, port, script, scriptArgs };
+}
